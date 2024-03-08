0120ff303a2b983788cc40f33a674df4e34361d6
Adrian Weetman <adrian.weetman@version1.com>
2024-03-08 14:53:06 +0000

    playing with retries

--- controls/example.rb
+++ controls/example.rb
@@ -19,6 +19,6 @@ end
 control 'check_error_logs' do
   # Check if there are any error logs
   describe command('ls /root -l | wc -l') do
-    its('stdout') { "3" }
+    its('stdout') { should eq "3" }
   end
 end
