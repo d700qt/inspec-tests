76589b5fb2c5a72ac9acecaf5c343e5157246791
Adrian Weetman <adrian.weetman@version1.com>
2024-03-08 20:10:37 +0000

    adding tests

--- files/script.sh
+++ files/script.sh
@@ -1,1 +1,26 @@
-ls /root -l | wc -l | tr -d "\\\\n"
+#!/bin/bash
+
+# Set the maximum number of retries
+max_retries=2
+
+# Set the wait time (in seconds) between retries
+wait_time=2
+
+# Your command goes here
+your_command='ls /root -l | wc -l | tr -d "\\\\n"'
+
+# Retry loop
+for ((i = 1; i <= max_retries; i++)); do
+    # Execute the command
+    eval $your_command
+    return_code=$?
+    echo "return code: $return_code"
+
+    # Check the exit status
+    if [ $return_code -eq 0 ]; then
+        echo "Command succeeded!"
+        break
+    else
+        echo "Command failed. Retrying in $wait_time seconds..."
+        sleep $wait_time
+    fi
